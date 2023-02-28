917e3278dc09cacf1d2141c8d4291c6ec38b09fd
realmojo88 <strikers1999@naver.com>
2023-02-28 15:51:38 +0900

    dgdg

--- pages/[category]/index.vue
+++ pages/[category]/index.vue
@@ -4,6 +4,8 @@
 
     <a href="/category/link" target="_self">페이지1</a>
     <a href="/category/link2" target="_self">페이지2</a>
+    <a href="/category/link3" target="_self">페이지4</a>
+    <a href="/category/link4" target="_self">페이지5</a>
   </main>
 </template>
 
