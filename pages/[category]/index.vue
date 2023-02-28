f32f2a88a55d60ba5973f8fc4249c6f79e3dff10
realmojo88 <strikers1999@naver.com>
2023-02-28 14:03:11 +0900

    update

--- pages/[category]/index.vue
+++ pages/[category]/index.vue
@@ -1,79 +1,10 @@
 <template>
   <main>
-    <a-layout class="site-layout">
-      <a-row>
-        <a-col
-          :xs="{ span: 24 }"
-          :sm="{ span: 24 }"
-          :md="{ span: 18 }"
-          :lg="{ span: 18 }"
-          :xl="{ span: 18 }"
-          :xxl="{ span: 18 }"
-        >
-          <a-row
-            class="pt-4 pb-4 px-2"
-            :gutter="8"
-            style="margin-left: 0, margin-right: 0"
-          >
-            <Items :items="items" />
-          </a-row>
-          <div class="text-center mb-4">
-            <a-button class="btn-mindpang" size="large" @click="doMoreItem">
-              더보기
-            </a-button>
-          </div>
-        </a-col>
-        <a-col
-          :xs="{ span: 24 }"
-          :sm="{ span: 24 }"
-          :md="{ span: 6 }"
-          :lg="{ span: 6 }"
-          :xl="{ span: 6 }"
-          :xxl="{ span: 6 }"
-        >
-          <Sidebar
-            :recentlyItems="recentlyItems"
-            :popularItems="popularItems"
-          />
-        </a-col>
-      </a-row>
-    </a-layout>
+    이곳은 카테고리 페이지 입니다.
+
+    <NuxtLink href="/category/link" target="_self">페이지1</NuxtLink>
+    <NuxtLink href="/category/link2" target="_self">페이지2</NuxtLink>
   </main>
 </template>
 
-<script setup>
-const route = useRoute();
-const runtimeConfig = useRuntimeConfig();
-const items = ref([]);
-const recentlyItems = ref([]);
-const popularItems = ref([]);
-const page = ref(1);
-
-const category = computed(() => {
-  return route.params.category ? route.params.category : "all";
-});
-
-const url = `${runtimeConfig.BASE_URL}/mind/main.php?category=${category.value}`;
-const { data } = await useFetch(url, {
-  key: "main",
-  method: "get",
-});
-
-const d = JSON.parse(data._rawValue);
-items.value = d.items;
-recentlyItems.value = d.recentlyItems;
-popularItems.value = d.popularItems;
-
-const doMoreItem = async () => {
-  const url = `/api/mind/list?category=${category.value}&page=${page.value}`;
-  const { data } = await useFetch(url, {
-    key: "moreList",
-    method: "get",
-  });
-  const moreItems = data._rawValue.data;
-  if (moreItems.length) {
-    page.value += 1;
-    items.value = items.value.concat(moreItems);
-  }
-};
-</script>
+<script setup></script>
